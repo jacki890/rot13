d0850c5874f123b37c535ed809a38959e9cb8852
jacki890 <jacki890@yahoo.com>
2011-03-11 14:06:41 -0500

    Lots of comments in rot13.rb

--- rot13/rot13.rb
+++ rot13/rot13.rb
@@ -1,3 +1,4 @@
+# creating a method
 def make_rot13_line(a_line)
   a_line
 end
@@ -7,6 +8,8 @@ in_file = File.open("/home/jacki/apprentice_work/rot13/original.txt", "r")
 
 # read the lines of the external file into an array called "lines"
 lines = in_file.readlines
+# added puts to see what lines is at this point
+puts lines
 
 # close the file
 in_file.close
@@ -17,6 +20,8 @@ out_file = File.open("/home/jacki/apprentice_work/rot13/rot13ed.txt", "w")
 # for each item in the lines array, write the array item (line) to the output file
 lines.each do |line|
   rot13_line = make_rot13_line(line)
+# adding this puts to see exactly what rot13_line is at this point
+# puts rot13_line
   out_file.write(rot13_line)
 end
 
@@ -24,6 +29,7 @@ end
 out_file.close
 
 # NEXT: read each line into an array. Once EOF, write all into output file.
+
 # alphabet = ("a".."z").to_a
 
 
