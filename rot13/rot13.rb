d2dddf7f629a5716ce5710da6186dd09644ae6dc
jacki890 <jacki890@yahoo.com>
2011-03-11 11:06:28 -0500

    added comments explaining what each part of the rot13 program is doing

--- rot13/rot13.rb
+++ rot13/rot13.rb
@@ -1,18 +1,29 @@
-in_file = File.open("/home/jacki/apprentice_work/rot13/original.txt", "r")
-
-lines = in_file.readlines
-
-in_file.close
-
-out_file = File.open("/home/jacki/apprentice_work/rot13/rot13ed.txt", "w")
-
-lines.each do |line|
-  out_file.write(line)
+def make_rot13_line(a_line)
+  a_line
 end
 
+# open the external file in read mode.
+in_file = File.open("/home/jacki/apprentice_work/rot13/original.txt", "r")
+
+# read the lines of the external file into an array called "lines"
+lines = in_file.readlines
+
+# close the file
+in_file.close
+
+# open a file for the output in write mode, and erase any previous data in the file.
+out_file = File.open("/home/jacki/apprentice_work/rot13/rot13ed.txt", "w")
+
+# for each item in the lines array, write the array item (line) to the output file
+lines.each do |line|
+  rot13_line = make_rot13_line(line)
+  out_file.write(rot13_line)
+end
+
+#close the output file
 out_file.close
 
-# read each line into an array. Once EOF, write all into output file.
+# NEXT: read each line into an array. Once EOF, write all into output file.
 # alphabet = ("a".."z").to_a
 
 
