bde2461e5198c2f6463f2c198ff9c112d3eca626
Pairing Machine <pairing@edgecase.com>
2011-03-21 11:21:56 -0400

    first commit to rot13.rb from MacBook. Back to start.

--- rot13.rb
+++ rot13.rb
@@ -4,24 +4,21 @@ def make_rot13_line(a_line)
 end
 
 # open the external file in read mode.
-in_file = File.open("/home/jacki/apprentice_work/rot13/original.txt", "r")
+in_file = File.open("/Users/jackikeys/Dropbox/apprentice_work/rot13/original.txt", "r")
 
 # read the lines of the external file into an array called "lines"
 lines = in_file.readlines
 # added puts to see what lines is at this point
-puts lines
 
 # close the file
 in_file.close
 
 # open a file for the output in write mode, and erase any previous data in the file.
-out_file = File.open("/home/jacki/apprentice_work/rot13/rot13ed.txt", "w")
+out_file = File.open("/Users/jackikeys/Dropbox/apprentice_work/rot13/output.txt", "w")
 
 # for each item in the lines array, write the array item (line) to the output file
 lines.each do |line|
   rot13_line = make_rot13_line(line)
-# adding this puts to see exactly what rot13_line is at this point
-# puts rot13_line
   out_file.write(rot13_line)
 end
 
