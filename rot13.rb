fd9238163335bd9188e76ffafebd35a010a1745d
Pairing Machine <pairing@edgecase.com>
2011-03-28 16:19:19 -0400

    making progress with each_byte

--- rot13.rb
+++ rot13.rb
@@ -1,32 +1,26 @@
 def make_rot13_line(a_line)
-  new_line = ''
-  a_line.each_char {|c| }
-  new_line
+  variable = ''
+  a_line.each_byte do |c|
+    variable << c + 13
+  end
+  variable
 end
 
-# open the external file in read mode.
+def rot13_one_char(char)
+  char
+end
+
 in_file = File.open("/Users/jackikeys/Dropbox/apprentice_work/rot13/original.txt", "r")
 
-# read the lines of the external file into an array called "lines"
 lines = in_file.readlines
 
-# close the file
 in_file.close
 
-# open a file for the output in write mode, and erase any previous data in the file.
 out_file = File.open("/Users/jackikeys/Dropbox/apprentice_work/rot13/output.txt", "w")
 
-# for each item in the lines array, write the array item (line) to the output file
 lines.each do |line|
   rot13_line = make_rot13_line(line)
   out_file.write(rot13_line)
 end
 
-#close the output file
 out_file.close
-
-# NEXT: read each line into an array. Once EOF, write all into output file.
-
-# alphabet = ("a".."z").to_a
-
-
